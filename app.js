fb52b11996c002805eb758e7d43a96bcaf5e671e
Shibajyoti Debbarma <shibajyotidebbarma@gmail.com>
2019-01-29 11:49:10 +0530

    Push Starter Stage Files

---
+++ app.js
@@ -0,0 +1,63 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he qhishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+var scores, roundScore, activePlayer;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+dice = Math.floor(Math.random()*6)+1;
+
+
+/******************
+TWO WAYS OF DOM MANIPULATION :
+*******************/
+
+//1.
+document.querySelector('#current-' + activePlayer).textContent = dice;
+
+//2.
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+
+//var x = document.querySelector('#score-0').textContent;  //GETTER 
+
+
+document.querySelector('.dice').style.display = 'none';
+
+
+
+//DONE TILL 48
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
