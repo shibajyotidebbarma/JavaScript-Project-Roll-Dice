44cc8ad734909e230bda6ca291c6673df46d4a12
Shibajyoti Debbarma <shibajyotidebbarma@gmail.com>
2019-01-29 12:40:31 +0530

    Add Game rule to change between the player turns when they encounter '1' on dice value

--- app.js
+++ app.js
@@ -45,16 +45,40 @@ document.getElementById('current-1').textContent = '0';
 document.querySelector('.btn-roll').addEventListener('click', function(){
 //ANONYMOUS FUNCTION
 
-var dice = Math.floor(Math.random()*6) +1;
+    var dice = Math.floor(Math.random()*6) +1;
 
-var diceDOM = document.querySelector('.dice');
-diceDOM.style.display = 'block';
-diceDOM.src = 'dice-' + dice + '.png';
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    
+    if (dice !== 1){
+        //add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    }
+    else{
+        //next player gets turn
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore= 0;
+        
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        
+        
+        //document.querySelector('.player-0-panel').classList.remove('active');
+        //document.querySelector('.player-1-panel').classList.add('active');
+        
+          document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        
+        document.querySelector('.dice').style.display = 'none';
+        
+    }
+    
 
 });
 
 
-//TILL 49
 
 
 
