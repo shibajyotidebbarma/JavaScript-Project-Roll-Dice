939a5238a7a5c546db228c7a8ec4a92fdcd33a0e
Shibajyoti Debbarma <shibajyotidebbarma@gmail.com>
2019-01-29 12:56:17 +0530

    Update the Hold Button EventListener to update the Global Score and declare the winner.

--- app.js
+++ app.js
@@ -58,7 +58,41 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     }
     else{
         //next player gets turn
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        nextPlayer();
+        
+    }
+    
+
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    
+    
+    scores[activePlayer] += roundScore;
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    
+    if (scores[activePlayer] >= 20){
+        document.getElementById('name-' + activePlayer).textContent= "Winner!";
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+        document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
+    }
+    else{    
+    
+    nextPlayer();
+        
+    }
+    
+
+    
+});
+
+
+
+
+
+function nextPlayer(){
+     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
         roundScore= 0;
         
         document.getElementById('current-0').textContent = '0';
@@ -72,23 +106,11 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
         document.querySelector('.player-1-panel').classList.toggle('active');
         
         document.querySelector('.dice').style.display = 'none';
-        
-    }
-    
-
-});
+}
 
 
 
-
-
-
-
-
-
-
-
-
+//TILL 51
 
 
 
